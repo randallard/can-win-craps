bc5adca1169424020c17896900423836d6588b84
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2023-12-24 13:52:33 -0800

    works

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -2,12 +2,22 @@
   (:require
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]))
-(defsc Person [this {:person/keys [id name] :as props}]
-  {:query [:person/id :person/name]
+(defsc Strategy [this {:strategy/keys [id description] :as props}]
+  {:query [:strategy/id :strategy/description]
+   :ident :strategy/id
+   :initial-state {:strategy/id :param/id
+                   :strategy/description :param/description}}
+  (dom/div {:style {:marginBottom "20px"}} (dom/p {} description)))
+(def ui-strategy (comp/factory Strategy {:keyfn :strategy/id}))
+(defsc Person [this {:person/keys [id name strategies] :as props}]
+  {:query [:person/id :person/name {:person/strategies (comp/get-query Strategy)}]
    :ident :person/id
    :initial-state {:person/id :param/id
-                   :person/name :param/name}}
-  (dom/div {:style {:marginBottom "20px"}} (dom/h3 {} name)))
+                   :person/name :param/name
+                   :person/strategies [{:id 1 :description "On the pass line, make the minimum bet, before the come out roll"}]}}
+  (dom/div {:style {:marginBottom "20px"}}
+           (dom/h3 {} name)
+           (map ui-strategy strategies)))
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
 (defsc Root [this {:root/keys [people]}]
